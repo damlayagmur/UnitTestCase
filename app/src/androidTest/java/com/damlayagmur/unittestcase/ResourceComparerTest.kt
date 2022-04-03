63dd3c8e4b9e6515d4cb4b60c28508a3aac28932
damlayagmur <dmlygmr@windowslive.com>
2022-04-03 18:52:22 +0300

    unit test for ResourceComparer AndroidTest

---
+++ app/src/androidTest/java/com/damlayagmur/unittestcase/ResourceComparerTest.kt
@@ -0,0 +1,34 @@
+package com.damlayagmur.unittestcase
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setup() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Unit Test Case")
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Damla")
+
+        assertThat(result).isFalse()
+    }
+
+}
